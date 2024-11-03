258357c023eef3506c1ef477daec21bec217a1d6
Youjia Li <lyoujia2021@gmail.com>
2024-11-03 15:17:06 -0600

    add another property

--- elemnet/cpu_run.sh
+++ elemnet/cpu_run.sh
@@ -1,3 +1,11 @@
+# Set CUDA to use CPU only
 export CUDA_VISIBLE_DEVICES="-1"
-python dl_regressors_torch.py --config_file ./sample/example_alignn_mbj_bandgap_local.config
-python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_mbj_bandgap_local.config
+
+# Define the list of properties
+# props=("mbj_bandgap" "Tc_supercon")
+props=("Tc_supercon")
+# Loop through each property in the list
+for prop in "${props[@]}"; do 
+    python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
+    python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
+done
