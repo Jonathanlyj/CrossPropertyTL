a5af9614c0c5abbd90f0b33099f3838afe93dc90
Youjia Li <lyoujia2021@gmail.com>
2024-11-03 10:33:08 -0600

    modified config file

--- elemnet/gpu_run.sh
+++ elemnet/gpu_run.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES="-1"
+unset CUDA_VISIBLE_DEVICES
 python dl_regressors_torch.py --config_file ./sample/example_alignn_mbj_bandgap_local.config
 python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_mbj_bandgap_local.config
---
+++ init.sh
@@ -0,0 +1,4 @@
+pip install -r torch_requirements.txt
+cd data/
+wget https://figshare.com/s/8629d471a5ad6b063465 -O tl_dataset.zip
+unzip tl_dataset.zip -d tl_dataset
