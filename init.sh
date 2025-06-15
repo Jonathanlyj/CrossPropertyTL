3032798acdbbcb44d554a479683c13db6ddb1194
Youjia Li <lyoujia2021@gmail.com>
2025-06-15 14:42:23 -0500

    update dataset link

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 pip install -r torch_requirements.txt
 cd data/
-wget https://figshare.com/ndownloader/articles/27569631/versions/1 -O tl_dataset.zip
+wget https://figshare.com/ndownloader/articles/27569631/versions/2 -O tl_dataset.zip
 unzip tl_dataset.zip -d tl_dataset
