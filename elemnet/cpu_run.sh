2a50e8bfbcb6d1b79268c73e88c42ab93ce618aa
Youjia Li <lyoujia2021@gmail.com>
2025-06-19 01:37:54 -0500

    add model inference command

--- elemnet/cpu_run.sh
+++ elemnet/cpu_run.sh
@@ -4,9 +4,16 @@ export CUDA_VISIBLE_DEVICES="-1"
 # Define the list of properties
 # props=("mbj_bandgap" "Tc_supercon")
 props=("mbj_bandgap")
-# Loop through each property in the list
+# Loop through each property in the list and retrain & inference the model
 for prop in "${props[@]}"; do 
     # python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
     # python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
     python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop".config    
 done
+
+
+# Inference on saved model
+for prop in "${props[@]}"; do 
+    python dl_regressors_torch_predict.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_mbj_bandgap.config --model_path model/alignn_matbert-base-cased_robo_prop_mbj_bandgap/model_1024Rx4D-512Rx3D-256Rx3D-128Rx3D-64Rx2-32Rx1-1_20250614_193231.pt    
+done
+
--- elemnet/gpu_run.sh
+++ elemnet/gpu_run.sh
@@ -1,5 +1,4 @@
 unset CUDA_VISIBLE_DEVICES
-export CUDA_VISIBLE_DEVICES="1"
 # Define the list of properties
 # props=("mbj_bandgap" "Tc_supercon")
 props=("Tc_supercon")
@@ -9,3 +8,9 @@ for prop in "${props[@]}"; do
     # python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
     python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop".config
 done
+
+
+for prop in "${props[@]}"; do 
+    python dl_regressors_torch_predict.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_mbj_bandgap.config --model_path model/alignn_matbert-base-cased_robo_prop_mbj_bandgap/model_1024Rx4D-512Rx3D-256Rx3D-128Rx3D-64Rx2-32Rx1-1_20250614_193231.pt    
+done
+
