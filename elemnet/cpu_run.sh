5914fc1e38951ee9554d5410596234b826712bf4
Youjia Li <lyoujia2021@gmail.com>
2025-06-19 01:32:22 -0500

    add local run option

--- elemnet/cpu_run.sh
+++ elemnet/cpu_run.sh
@@ -1,11 +1,12 @@
 # Set CUDA to use CPU only
 export CUDA_VISIBLE_DEVICES="-1"
-
+# conda activate tl_torch
 # Define the list of properties
 # props=("mbj_bandgap" "Tc_supercon")
-props=("Tc_supercon")
+props=("mbj_bandgap")
 # Loop through each property in the list
 for prop in "${props[@]}"; do 
-    python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
-    python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
+    # python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
+    # python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
+    python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop".config    
 done
--- elemnet/gpu_run.sh
+++ elemnet/gpu_run.sh
@@ -1,9 +1,11 @@
 unset CUDA_VISIBLE_DEVICES
+export CUDA_VISIBLE_DEVICES="1"
 # Define the list of properties
 # props=("mbj_bandgap" "Tc_supercon")
 props=("Tc_supercon")
 # Loop through each property in the list
 for prop in "${props[@]}"; do 
-    python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
-    python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
+    # python dl_regressors_torch.py --config_file ./sample/example_alignn_"$prop"_local.config
+    # python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop"_local.config
+    python dl_regressors_torch.py --config_file ./sample/example_alignn_matbert-base-cased_robo_prop_"$prop".config
 done
