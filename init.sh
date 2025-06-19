820e5f246ada56d434626fab9e4bfa210f57f55f
Youjia Li <lyoujia2021@gmail.com>
2025-06-19 17:30:51 -0500

    add models downloading

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@ pip install -r torch_requirements.txt
 cd data/
 wget https://figshare.com/ndownloader/articles/27569631/versions/2 -O tl_dataset.zip
 unzip tl_dataset.zip -d tl_dataset
+cd ..
+wget https://figshare.com/ndownloader/articles/29367467?private_link=1be8bdcbd8db1ff52d5c -O models.zip
+unzip model.zip
+
